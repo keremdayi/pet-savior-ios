a98199d1efbf4aaa0de1528ddb801a2189b43a75
Kerem Dayı <arifkeremdayi@gmail.com>
2017-02-08 20:56:05 +0300

    addViewController edit

--- PetSavior/AddViewController.swift
+++ PetSavior/AddViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Alamofire
+import CoreLocation
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
     
+    let manager = CLLocationManager()
+    var latitude : Any?
+    var longitude : Any?
+    var image : UIImage?
     @IBOutlet weak var addImageButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
@@ -23,6 +29,35 @@ class AddViewController: UIViewController {
         self.titleTextField.layer.borderWidth = 1.0
         self.titleTextField.layer.cornerRadius = 5.0
         // Do any additional setup after loading the view.
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.requestLocation()
     }
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("received location info")
+        let userLocation:CLLocation = locations[0]
+        let longitude = userLocation.coordinate.longitude
+        let latitude = userLocation.coordinate.latitude
+        self.latitude = latitude
+        self.longitude = longitude
+        }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("failed location info")
+    }
+    @IBAction func photoButton(sender: AnyObject){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            self.image = UIImageJPEGRepresentation(imagePicker)
+        }
+    }
+    @IBAction func post(){
+        Alamofire.upload(image, to : "http://petsavior.gokhanakkurt.com/posts", parameters : ["longitude" : self.longitude, "latitude" : self.latitude, "title" : titleTextField.text,"description" : descriptionTextView.text]){ response in
+            debugPrint(response)
+        }
+    }
 }
